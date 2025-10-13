61238132943aa2d4c2b7595d45add8839f117296
qyzh <mrveranda@gmail.com>
2025-10-13 15:57:44 +0700

    feat: fix

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -8,4 +8,5 @@ vim.g.copilot_filetypes = {
 }
 vim.g.copilot_no_tab_map = true
 vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+vim.g.loaded_netrwPlugin = 1
 require("config.lazy")
