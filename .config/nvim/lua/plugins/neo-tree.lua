75f11e88ade68751f588f1cb55059a9ea422a814
qyzh <mrveranda@gmail.com>
2025-10-13 15:57:25 +0700

    fix: Config Neotree w/ vimExplo

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -1,48 +1,53 @@
--- return {
--- 	-- File explorer utama
--- 	{
--- 		"nvim-neo-tree/neo-tree.nvim",
--- 		branch = "v3.x",
--- 		dependencies = {
--- 			"nvim-lua/plenary.nvim",
--- 			"MunifTanjim/nui.nvim",
--- 			"nvim-tree/nvim-web-devicons",
--- 			"s1n7ax/nvim-window-picker", -- integrasi langsung
--- 		},
--- 		cmd = "Neotree", -- lazy-load hanya saat dipanggil
--- 		keys = {
--- 			{ "<leader>e", "<cmd>Neotree toggle<cr>", desc = "Explorer (Neo-tree)" },
--- 			{ "<leader>o", "<cmd>Neotree focus<cr>", desc = "Focus Neo-tree" },
--- 		},
--- 		config = function()
--- 			require("neo-tree").setup({
--- 				close_if_last_window = true, -- tutup kalau cuma tinggal Neo-tree
--- 				popup_border_style = "rounded",
--- 				enable_git_status = true,
--- 				enable_diagnostics = true,
--- 				default_component_configs = {
--- 					indent = { padding = 0, with_markers = true },
--- 					icon = { folder_closed = "", folder_open = "", default = "" },
--- 					git_status = { symbols = { added = "✚", modified = "", deleted = "✖" } },
--- 				},
--- 				window = {
--- 					width = 30,
--- 					mappings = {
--- 						["<cr>"] = "open_with_window_picker", -- integrasi window picker
--- 						["l"] = "open",
--- 						["h"] = "close_node",
--- 					},
--- 				},
--- 			})
--- 		end,
--- 	},
+-- Nonaktifkan netrw agar tidak bentrok dengan Neo-tree
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 
--- 	-- LSP aware file operations (rename, move, etc.)
--- 	{
--- 		"antosha417/nvim-lsp-file-operations",
--- 		dependencies = { "nvim-neo-tree/neo-tree.nvim" },
--- 		config = function()
--- 			require("lsp-file-operations").setup()
--- 		end,
--- 	},
--- }
+return {
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"s1n7ax/nvim-window-picker",
+		},
+		cmd = "Neotree",
+		keys = {
+			{ "<leader>j", "<cmd>Neotree toggle<cr>", desc = "Explorer (Neo-tree)" },
+			{ "<leader>o", "<cmd>Neotree focus<cr>", desc = "Focus Neo-tree" },
+		},
+		config = function()
+			require("neo-tree").setup({
+				close_if_last_window = true,
+				popup_border_style = "rounded",
+				enable_git_status = true,
+				enable_diagnostics = true,
+				default_component_configs = {
+					indent = { padding = 0, with_markers = true },
+					icon = { folder_closed = "", folder_open = "", default = "" },
+					git_status = { symbols = { added = "✚", modified = "", deleted = "✖" } },
+				},
+				filesystem = {
+					hijack_netrw_behavior = "open_default",
+				},
+				window = {
+					width = 30,
+					mappings = {
+						["<cr>"] = "open_with_window_picker",
+						["l"] = "open",
+						["h"] = "close_node",
+					},
+				},
+			})
+		end,
+	},
+
+	{
+		"antosha417/nvim-lsp-file-operations",
+		dependencies = { "nvim-neo-tree/neo-tree.nvim" },
+		config = function()
+			require("lsp-file-operations").setup()
+		end,
+	},
+}
